a9a00bc9e1efa6523fa30916f52dcf97fe045dc5
Sean Reed <seanreed1111@gmail.com>
2023-01-10 17:21:48 -0500

    updates test to see if the pytorch model is pickled

--- src/test.py
+++ src/test.py
@@ -1,7 +1,7 @@
 # import seaborn, logging, matplotlib, pandas, torch, torchvision, torchaudio, cv2, mlflow, torchmetrics, nibabel
 from loggers import create_python_logger
 from pathlib import Path
-
+import pickletools
 import torch, torchvision
 from omegaconf import OmegaConf
 from models.pcrlv2_model_3d import PCRLv23d
@@ -12,11 +12,18 @@ if __name__ == "__main__":
     args = OmegaConf.from_cli()
     pylogger.debug(f"{args}")
     model = PCRLv23d()
+    pylogger.debug(f"model= \n {model}\n")
     model_name = "simance_multi_crop_luna_pretask_1.0_240.pt"
     weight_path = Path(str(args.base_weight_path), model_name)
-    model_dict = torch.load(weight_path)["state_dict"]
-    pylogger.debug(f"model= \n {model}\n")
-    pylogger.debug(f"state dict= \n {state_dict}\n")
-    model.load_state_dict(torch.load(model_dict))
-    pylogger.debug("model loaded successfully")
-    pylogger.debug(f"{model}")
+    with open(weight_path, "rb") as f:
+        pickle = f.read()
+        output = pickletools.genops(pickle)
+        opcodes = []
+        for opcode in output:
+            opcodes.append(opcode[0])
+        pylogger.debug(f"opcodes[0].name:{opcodes[0].name}")
+        pylogger.debug(f"opcodes[-1].name:{opcodes[-1].name}")
+    # model_dict = torch.load(weight_path, map_location=torch.device('cpu'))["state_dict"]
+
+    # # pylogger.debug(f"model dict= \n {model_dict}\n")
+    # model.load_state_dict(torch.load(model_dict)) #error here.
